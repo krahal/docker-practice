4cbbdc5bea00003f27406159034246e770b14561
Karn Rahal <karn.rahal@alumni.ubc.ca>
2019-04-08 12:13:42 -0700

    set up debugging

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Karn'));
+app.get('/', (req, res) => {
+  const value = 5;
+  res.send('Hello Karn')
+});
 
 app.listen(3000, () => {
   console.log("Express server started on port 3000.");
-})
+});
